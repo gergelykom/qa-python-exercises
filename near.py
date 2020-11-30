626a8fe965e324f482ba0e0afb4d4a441b70b5cf
gergely_komuves <70955431+gergelykom@users.noreply.github.com>
2020-11-30 16:35:48 +0000

    Add files via upload

---
+++ near.py
@@ -0,0 +1,24 @@
+near1 = str(input('Word1: '))
+
+near2 = str(input('Word2: '))
+
+near = ''
+nearer = ''
+
+lengt = len(near2) if len(near1)<len(near2) else len(near1)
+
+for i in range(lengt):
+    word1 = near1[i:i + 1]
+    word2 = near2[i:i+1]
+    if word1 != word2:
+        near += word1
+        nearer += word2
+
+if len(near) + len(nearer) <= 3:
+    print('True', near1, near2)
+else:
+    print('False')
+
+
+
+
