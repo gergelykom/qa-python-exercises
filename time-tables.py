dc7ef17d4868d7ed13fcdb278f150da4c20dec60
gergely_komuves <70955431+gergelykom@users.noreply.github.com>
2020-12-01 15:55:06 +0000

    Delete time-tables.py

--- time-tables.py
+++
@@ -1,9 +0,0 @@
-num = int(input('Please enter your number: '))
-
-def tables(num):
-    for top in range(1, num + 1):
-        for down in range(1, num +1):
-            print(top * down, end='\t')
-        print()
-
-tables(num)
